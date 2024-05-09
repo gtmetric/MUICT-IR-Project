9c1664cb8c8832e3bc18adf09a25bae5d4bd2697
gtmetric <ly.soksedtha81@gmail.com>
2024-05-09 11:47:16 +0800

    Add requirements and explicitly call tkinter

--- elasticsearchGUI.py
+++ elasticsearchGUI.py
@@ -1,13 +1,15 @@
-from tkinter import *
+import tkinter
 from elasticsearch import Elasticsearch
 
 # Create a window
-master = Tk() 
+master = tkinter.Tk()
 master.title('Elasticsearch Interface')
-master.geometry('800x600') 
+master.geometry('800x600')
 
-# Retrieve the elasticsearch results
+
 def elasticsearch(query):
+    # Retrieve the elasticsearch results
+
     result = ''
     es = Elasticsearch()
     res = es.search(
@@ -35,45 +37,52 @@ def elasticsearch(query):
     result = ('Found %d Results:' % res['hits']['total']['value']) + '\n'
     for hit in res['hits']['hits']:
         hit['_source']['keywords'] = str(hit['_source']['keywords'])
-        hit['_source']['keywords'] = hit['_source']['keywords'].replace('[','')
-        hit['_source']['keywords'] = hit['_source']['keywords'].replace(']','')
-        hit['_source']['keywords'] = hit['_source']['keywords'].replace('\'','')
-        
+        hit['_source']['keywords'] = hit['_source']['keywords'].replace(
+            '[', '')
+        hit['_source']['keywords'] = hit['_source']['keywords'].replace(
+            ']', '')
+        hit['_source']['keywords'] = hit['_source']['keywords'].replace(
+            '\'', '')
+
         result += '\nScore: ' + str(hit['_score'])
-        result += ("\nTitle: %(title)s\nAuthor: %(author)s\nYear: %(year)s\nOrigin: %(origin)s\nGenre: %(genre)s\nKeywords: %(keywords)s\nURL: %(url)s\n" % hit["_source"])
-        # result += ("\nTitle: %(title)s\nAuthor: %(author)s\nYear: %(year)s\nOrigin: %(origin)s\nGenre: %(genre)s\nURL: %(url)s\n" % hit["_source"])
+        result += ("\nTitle: %(title)s\nAuthor: %(author)s\nYear: %(year)s\nOrigin: %(origin)s\nGenre: %(genre)s\nKeywords: %(keywords)s\nURL: %(url)s\n" %
+                   hit["_source"])
     return result
 
 # Put the elasticsearch results to the window
+
+
 def showSearchResults():
     query = query_text.get()
     result = elasticsearch(query)
     results_label.delete('1.0', END)
     results_label.insert(END, result)
-  
+
+
 # Search label widget
-search_label = Label(master, text = "Keyword: ") 
-search_label.place(relx = 0.23, y = 12, anchor = NW) 
+search_label = tkinter.Label(master, text="Keyword: ")
+search_label.place(relx=0.23, y=12, anchor=tkinter.NW)
 
-# Query text field widget 
-query = StringVar()
-query_text = Entry(master, textvariable=query, width = 50) 
-query_text.place(relx = 0.5, y = 5, rely = 0.028, anchor = CENTER)
+# Query text field widget
+query = tkinter.StringVar()
+query_text = tkinter.Entry(master, textvariable=query, width=50)
+query_text.place(relx=0.5, y=5, rely=0.028, anchor=tkinter.CENTER)
 
-# Search button widget 
-search_button = Button(master, text = "Search", command = showSearchResults) 
-search_button.place(relx = 0.77, y = 9, anchor = NE)
+# Search button widget
+search_button = tkinter.Button(
+    master, text="Search", command=showSearchResults)
+search_button.place(relx=0.77, y=9, anchor=tkinter.NE)
 
-# Results text field widget 
-results = StringVar()
-results_label = Text(master, width = 80, height = 30)
+# Results text field widget
+results = tkinter.StringVar()
+results_label = tkinter.Text(master, width=80, height=30)
 results.set('Results will be shown here.')
-results_label.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+results_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 # Scrollbar of the results text field
-scrollbar = Scrollbar(master)
-scrollbar.pack(side=RIGHT, fill=Y)
+scrollbar = tkinter.Scrollbar(master)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 results_label.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=results_label.yview)
 
-master.mainloop() 
+master.mainloop()
